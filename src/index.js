424f665da95f67f26ed8e80b9dbe4f16c6b89abc
Charlie-dev-test <mailbox.boyko@gmail.com>
2021-03-13 18:23:37 +0300

    feat: add task resolve

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    const config = bracketsConfig.map(br => br.join(''));
+    let count = 0;
+    while (count != bracketsConfig.length){
+        count = 0;
+        config.forEach(function(conf){
+            if(str.includes(conf)){
+                str = str.replace(conf, '');
+            }else{
+                count++;
+            }
+        });
+    }
+    if(str.length == 0){
+        return true;
+    }else {
+        return false;
+    }
 }
